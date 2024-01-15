ba0650692c97bb448bc50e819aaacb44ab0c66f4
Eunice <jes.e.0927@gmail.com>
2024-01-15 11:40:29 +0900

    Initial Commit

---
+++ PJ3T3_Postie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PJ3T3_Postie
+//
+//  Created by Eunsu JEONG on 1/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PJ3T3_Postie/PJ3T3_PostieApp.swift
@@ -0,0 +1,17 @@
+//
+//  PJ3T3_PostieApp.swift
+//  PJ3T3_Postie
+//
+//  Created by Eunsu JEONG on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct PJ3T3_PostieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
