774c4e2204926c6fb8c26f9570d4b19bbd02cef3
hjsupernova <kem08120506@gmail.com>
2024-01-17 15:32:28 +0900

    Design: AddLetterView 기본 UI 디자인 구현

--- PJ3T3_Postie/Core/Home/View/AddLetterView.swift
+++ PJ3T3_Postie/Core/Home/View/AddLetterView.swift
@@ -11,10 +11,88 @@ struct AddLetterView: View {
     @StateObject private var addLetterViewModel = AddLetterViewModel()
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(hex: 0xF5F1E8)
+                .ignoresSafeArea()
+
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Text("보내는 사람")
+
+                    TextField("", text: $addLetterViewModel.sender)
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color(hex: 0x807E79), lineWidth: 1)
+                        )
+
+                    Text("받는 사람")
+
+                    TextField("", text: $addLetterViewModel.sender)
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color(hex: 0x807E79), lineWidth: 1)
+                        )
+
+                    Text("편지 날짜")
+
+                    DatePicker("날짜", selection: $addLetterViewModel.date, displayedComponents: .date)
+                        .labelsHidden()
+                        .environment(\.locale, Locale.init(identifier: "ko"))
+
+                    Text("편지 사진")
+
+                    ScrollView(.horizontal) {
+                        HStack(spacing: 8) {
+                            Button {
+                                // showConfirmationDialog
+                            } label: {
+                                Image(systemName: "envelope.open.fill")
+                                    .padding()
+                                    .frame(width: 100, height: 100)
+                                    .background(
+                                        Color.gray.opacity(0.2),
+                                        in: RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                    )
+                            }
+                        }
+                    }
+                    .scrollIndicators(.never)
+                    
+                    Text("편지 내용")
+                    
+                    TextEditor(text: $addLetterViewModel.text)
+                        .scrollContentBackground(.hidden)
+                        .padding()
+                        .lineSpacing(5)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 280)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color(hex: 0x807E79), lineWidth: 1)
+                        )
+                }
+                .padding()
+            }
+        }
+        .navigationTitle("편지 등록")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbarBackground(Color(hex: 0xF5F1E8), for: .navigationBar)
+        .toolbar {
+            ToolbarItemGroup(placement: .topBarTrailing) {
+                Button {
+                    // 편지 저장하기
+                } label : {
+                    Text("완료")
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    AddLetterView()
+    NavigationStack {
+        AddLetterView()
+    }
 }
--- PJ3T3_Postie/Core/Home/ViewModel/AddLetterViewModel.swift
+++ PJ3T3_Postie/Core/Home/ViewModel/AddLetterViewModel.swift
@@ -8,5 +8,8 @@
 import Foundation
 
 class AddLetterViewModel: ObservableObject {
-    
+    @Published var sender: String = ""
+    @Published var receiver: String = ""
+    @Published var date: Date = .now
+    @Published var text: String = "사진을 등록하면 자동으로 편지 내용이 입력됩니다."
 }
