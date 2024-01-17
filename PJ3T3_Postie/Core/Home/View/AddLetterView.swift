77f91e738767eb0706abdd9b1dd4febfb0ae5706
hjsupernova <kem08120506@gmail.com>
2024-01-17 16:17:06 +0900

    Feat: 편지 사진 버튼 클릭 시 confirmationDialog 나오도록 구현

--- PJ3T3_Postie/Core/Home/View/AddLetterView.swift
+++ PJ3T3_Postie/Core/Home/View/AddLetterView.swift
@@ -58,7 +58,7 @@ struct AddLetterView: View {
                         ScrollView(.horizontal) {
                             HStack(spacing: 8) {
                                 Button {
-                                    // showConfirmationDialog
+                                    addLetterViewModel.showConfirmationDialog = true
                                 } label: {
                                     Image(systemName: "envelope.open.fill")
                                         .padding()
@@ -103,7 +103,6 @@ struct AddLetterView: View {
             .navigationBarTitleDisplayMode(.inline)
             .toolbarBackground(Color(hex: 0xF5F1E8), for: .navigationBar)
             .toolbar {
-
                 ToolbarItemGroup(placement: .topBarTrailing) {
                     Button {
                         // 편지 저장하기
@@ -111,7 +110,23 @@ struct AddLetterView: View {
                         Text("완료")
                     }
                 }
+            }
+            .confirmationDialog("편지 사진 가져오기", isPresented: $addLetterViewModel.showConfirmationDialog) {
+                Button("카메라") {
+                    addLetterViewModel.imagePickerSourceType = .camera
+                    addLetterViewModel.showUIImagePicker = true
+                }
 
+                Button("앨범") {
+                    addLetterViewModel.imagePickerSourceType = .photoLibrary
+                    addLetterViewModel.showUIImagePicker = true
+                }
+
+                Button("스캐너") {
+
+                }
+            } message: {
+                Text("편지 사진 가져오기")
             }
         }
     }
--- PJ3T3_Postie/Core/Home/ViewModel/AddLetterViewModel.swift
+++ PJ3T3_Postie/Core/Home/ViewModel/AddLetterViewModel.swift
@@ -6,10 +6,15 @@
 //
 
 import Foundation
+import UIKit
 
 class AddLetterViewModel: ObservableObject {
     @Published var sender: String = ""
     @Published var receiver: String = ""
     @Published var date: Date = .now
     @Published var text: String = "사진을 등록하면 자동으로 편지 내용이 입력됩니다."
+    @Published var showConfirmationDialog: Bool = false
+    @Published var showUIImagePicker = false
+
+    var imagePickerSourceType: UIImagePickerController.SourceType = .camera
 }
