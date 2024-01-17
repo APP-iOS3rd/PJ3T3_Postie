229b67fff9ea24433bbe2859e8a97f465328839c
Eunice <jes.e.0927@gmail.com>
2024-01-17 11:14:53 +0900

    Feat: LoginInputView UI 구성

--- PJ3T3_Postie/Core/Login/View/LoginInputView.swift
+++ PJ3T3_Postie/Core/Login/View/LoginInputView.swift
@@ -7,12 +7,33 @@
 
 import SwiftUI
 
+//reusable component
 struct LoginInputView: View {
+    @Binding var text: String
+    let title: String
+    let placeholder: String
+    var isSecureField = false
+    //Colors
+    private let fontColor: Color = .black
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 12) {
+            Text(title)
+                .foregroundStyle(fontColor)
+                .fontWeight(.semibold)
+                .font(.footnote)
+
+            if isSecureField {
+                SecureField(placeholder, text: $text)
+            } else {
+                TextField(placeholder, text: $text)
+            }
+
+            Divider()
+        }
     }
 }
 
 #Preview {
-    LoginInputView()
+    LoginInputView(text: .constant(""), title: "Email Address", placeholder: "name@example.com")
 }
