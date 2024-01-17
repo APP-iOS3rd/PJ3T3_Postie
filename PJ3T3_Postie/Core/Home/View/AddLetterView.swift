1ea52ce719718cc7d7585d63a8107d2a354152c1
hjsupernova <kem08120506@gmail.com>
2024-01-17 16:07:37 +0900

    Feat: Keyboard dismiss button 추가

--- PJ3T3_Postie/Core/Home/View/AddLetterView.swift
+++ PJ3T3_Postie/Core/Home/View/AddLetterView.swift
@@ -8,84 +8,110 @@
 import SwiftUI
 
 struct AddLetterView: View {
+
+    enum Field: Hashable {
+        case sender
+        case receiver
+        case text
+    }
+
     @StateObject private var addLetterViewModel = AddLetterViewModel()
 
+    @FocusState private var focusField: Field?
+
     var body: some View {
-        ZStack {
-            Color(hex: 0xF5F1E8)
-                .ignoresSafeArea()
+        NavigationStack {
+            ZStack {
+                Color(hex: 0xF5F1E8)
+                    .ignoresSafeArea()
 
-            ScrollView {
-                VStack(alignment: .leading) {
-                    Text("보내는 사람")
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        Text("보내는 사람")
 
-                    TextField("", text: $addLetterViewModel.sender)
-                        .padding()
-                        .background(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color(hex: 0x807E79), lineWidth: 1)
-                        )
+                        TextField("", text: $addLetterViewModel.sender)
+                            .padding()
+                            .background(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color(hex: 0x807E79), lineWidth: 1)
+                            )
+                            .focused($focusField, equals: .sender)
 
-                    Text("받는 사람")
+                        Text("받는 사람")
 
-                    TextField("", text: $addLetterViewModel.sender)
-                        .padding()
-                        .background(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color(hex: 0x807E79), lineWidth: 1)
-                        )
+                        TextField("", text: $addLetterViewModel.receiver)
+                            .padding()
+                            .background(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color(hex: 0x807E79), lineWidth: 1)
+                            )
+                            .focused($focusField, equals: .receiver)
 
-                    Text("편지 날짜")
+                        Text("편지 날짜")
 
-                    DatePicker("날짜", selection: $addLetterViewModel.date, displayedComponents: .date)
-                        .labelsHidden()
-                        .environment(\.locale, Locale.init(identifier: "ko"))
+                        DatePicker("날짜", selection: $addLetterViewModel.date, displayedComponents: .date)
+                            .labelsHidden()
+                            .environment(\.locale, Locale.init(identifier: "ko"))
 
-                    Text("편지 사진")
+                        Text("편지 사진")
 
-                    ScrollView(.horizontal) {
-                        HStack(spacing: 8) {
-                            Button {
-                                // showConfirmationDialog
-                            } label: {
-                                Image(systemName: "envelope.open.fill")
-                                    .padding()
-                                    .frame(width: 100, height: 100)
-                                    .background(
-                                        Color.gray.opacity(0.2),
-                                        in: RoundedRectangle(cornerRadius: 10, style: .continuous)
-                                    )
+                        ScrollView(.horizontal) {
+                            HStack(spacing: 8) {
+                                Button {
+                                    // showConfirmationDialog
+                                } label: {
+                                    Image(systemName: "envelope.open.fill")
+                                        .padding()
+                                        .frame(width: 100, height: 100)
+                                        .background(
+                                            Color.gray.opacity(0.2),
+                                            in: RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                        )
+                                }
                             }
                         }
+                        .scrollIndicators(.never)
+
+                        Text("편지 내용")
+
+                        TextEditor(text: $addLetterViewModel.text)
+                            .scrollContentBackground(.hidden)
+                            .padding()
+                            .lineSpacing(5)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 280)
+                            .background(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color(hex: 0x807E79), lineWidth: 1)
+                            )
+                            .focused($focusField, equals: .text)
+                            .toolbar {
+                                ToolbarItemGroup(placement: .keyboard) {
+                                    Spacer()
+                                    Button {
+                                        focusField = nil
+                                    } label: {
+                                        Image(systemName: "keyboard.chevron.compact.down")
+                                    }
+                                }
+                            }
                     }
-                    .scrollIndicators(.never)
-                    
-                    Text("편지 내용")
-                    
-                    TextEditor(text: $addLetterViewModel.text)
-                        .scrollContentBackground(.hidden)
-                        .padding()
-                        .lineSpacing(5)
-                        .frame(maxWidth: .infinity)
-                        .frame(height: 280)
-                        .background(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color(hex: 0x807E79), lineWidth: 1)
-                        )
+                    .padding()
                 }
-                .padding()
             }
-        }
-        .navigationTitle("편지 등록")
-        .navigationBarTitleDisplayMode(.inline)
-        .toolbarBackground(Color(hex: 0xF5F1E8), for: .navigationBar)
-        .toolbar {
-            ToolbarItemGroup(placement: .topBarTrailing) {
-                Button {
-                    // 편지 저장하기
-                } label : {
-                    Text("완료")
+            .navigationTitle("편지 등록")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(Color(hex: 0xF5F1E8), for: .navigationBar)
+            .toolbar {
+
+                ToolbarItemGroup(placement: .topBarTrailing) {
+                    Button {
+                        // 편지 저장하기
+                    } label : {
+                        Text("완료")
+                    }
                 }
+
             }
         }
     }
