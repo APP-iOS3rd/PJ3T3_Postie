9e865141ae8c0a08dbb3ff534f033a889f5df56b
hjsupernova <kem08120506@gmail.com>
2024-01-17 17:06:44 +0900

    Feat: Vision 프레임워크 사용해 사용자의 이미지에서 텍스트를 인식할 TextRecognizer 클래스 생성

---
+++ PJ3T3_Postie/Core/Home/ViewModel/TextRecognizer.swift
@@ -0,0 +1,44 @@
+//
+//  TextRecognizer.swift
+//  PJ3T3_Postie
+//
+//  Created by KHJ on 2024/01/17.
+//
+
+import Foundation
+import UIKit
+import Vision
+
+class TextRecognizer {
+    let selectedImage: UIImage
+    var recognizedText = ""
+
+    init(selectedImage: UIImage) {
+        self.selectedImage = selectedImage
+    }
+
+    func recognizeText() {
+        guard let cgImage = selectedImage.cgImage else { return }
+
+        let requestHandler = VNImageRequestHandler(cgImage: cgImage)
+
+        let request = VNRecognizeTextRequest()
+        request.recognitionLanguages = ["ko-KR"]
+        request.recognitionLevel = .accurate
+        request.usesLanguageCorrection = true
+
+        do {
+            try requestHandler.perform([request])
+        } catch {
+
+        }
+    }
+
+    private func recognizeTextHandler(request: VNRequest, error: Error?) {
+        guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
+
+        recognizedText = observations.compactMap {
+            $0.topCandidates(1).first?.string
+        }.joined(separator: "\n")
+    }
+}
