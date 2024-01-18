f301f82af3b6f84979f254469b7f48a21111e145
burst-cannon <qlrmr111@naver.com>
2024-01-18 23:19:38 +0900

    Feat: 플로팅 버튼 생성

--- PJ3T3_Postie/App/PJ3T3_PostieApp.swift
+++ PJ3T3_Postie/App/PJ3T3_PostieApp.swift
@@ -26,7 +26,7 @@ struct YourApp: App {
   var body: some Scene {
     WindowGroup {
       NavigationView {
-        ContentView()
+        HomeView()
       }
     }
   }
--- PJ3T3_Postie/Core/Home/View/HomeView.swift
+++ PJ3T3_Postie/Core/Home/View/HomeView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct HomeView: View {
     @State private var search: String = ""
     @State private var isToggleOn = false
+    @State private var showAlert = false
     
     var body: some View {
         ZStack {
@@ -24,7 +25,30 @@ struct HomeView: View {
                     }
                     .searchable(text: $search)
                     .background(isToggleOn ? Color(hex: 0x1E1E1E) : Color(hex: 0xF5F1E8))
+                    
+                    Button(action: {
+                        showAlert.toggle()
+                    }, label: {
+                        ZStack {
+                            Circle()
+                                .foregroundStyle(isToggleOn ? Color(hex: 0xC9D1FF) : Color(hex: 0x979797))
+                                .frame(width:70,height:70)
+                            Image(systemName: "envelope")
+                        }
+                    })
+                    .foregroundStyle(Color.black)
+                    .imageScale(.large)
+                    .padding()
+                    .alert("편지 저장 하기", isPresented: $showAlert) {
+                        Button("편지 저장") {
+                        }
+                        Button("취소", role: .cancel) {
+                        }
+                    }
                 }
+                .preferredColorScheme(isToggleOn ? .dark : .light)
+                .navigationBarTitle("Postie")
+                .foregroundStyle(isToggleOn ? Color(hex: 0xD5D5D5) : Color(hex: 0x1e1e1e))
             }
         }
     }
