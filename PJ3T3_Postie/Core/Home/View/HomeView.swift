d15c7e9914cca8c073a6ad04389935685b7ba74f
burst-cannon <qlrmr111@naver.com>
2024-01-18 17:34:49 +0900

    Feat: 보낸/받은 메시지 규격함수 추가

--- PJ3T3_Postie/Core/Home/View/HomeView.swift
+++ PJ3T3_Postie/Core/Home/View/HomeView.swift
@@ -23,6 +23,117 @@ struct HomeView: View {
     }
 }
 
+func receiveLetterView(sender: String, date: String, receiver: String, isToggleOn: Bool) -> some View {
+    HStack {
+        HStack {
+            VStack(alignment: .leading) {
+                HStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 100, height: 35)
+                        .overlay(
+                            Text("보내는 사람")
+                                .foregroundStyle(Color(hex: 0x1e1e1e))
+                        )
+                        .foregroundStyle(Color(hex: 0x979797))
+                    
+                    Text("\(sender)")
+                        .foregroundStyle(Color(hex: 0x1e1e1e))
+                    
+                    Spacer()
+                    
+                    Image(systemName: "swift")
+                        .font(.title)
+                        .foregroundStyle(Color(hex: 0x1e1e1e))
+                }
+                
+                Text(" ")
+                
+                HStack {
+                    VStack (alignment: .leading) {
+                        Text(date)
+                            .foregroundStyle(Color(hex: 0x1e1e1e))
+                    }
+                    
+                    Spacer()
+                    
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 90, height: 35)
+                        .overlay(
+                            Text("받는 사람")
+                                .foregroundColor(.black)
+                        )
+                        .foregroundStyle(Color(hex: 0x979797))
+                    
+                    Text("\(receiver)")
+                        .foregroundStyle(Color(hex: 0x1e1e1e))
+                }
+            }
+        }
+        .padding()
+        .frame(width: 300)
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundStyle(Color(hex: 0xD1CEC7))
+        )
+        Spacer()
+    }
+}
+
+func sendLetterView(sender: String, date: String, receiver: String, isToggleOn: Bool) -> some View {
+    HStack {
+        Spacer()
+        HStack {
+            VStack(alignment: .leading) {
+                HStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 100, height: 35)
+                        .overlay(
+                            Text("보내는 사람")
+                                .foregroundColor(.black)
+                        )
+                        .foregroundStyle(Color(hex: 0x979797))
+                    
+                    Text("\(sender)")
+                        .foregroundStyle(Color(hex: 0x1e1e1e))
+                    
+                    Spacer()
+                    
+                    Image(systemName: "swift")
+                        .foregroundStyle(Color(hex: 0x1e1e1e))
+                        .font(.title)
+                }
+                
+                Text(" ")
+                
+                HStack {
+                    VStack (alignment: .leading) {
+                        Text(date)
+                    }
+                    
+                    Spacer()
+                    
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 90, height: 35)
+                        .overlay(
+                            Text("받는 사람")
+                                .foregroundColor(Color(hex: 0x1e1e1e))
+                        )
+                        .foregroundStyle(Color(hex: 0x979797))
+                    
+                    Text("\(receiver)")
+                        .foregroundStyle(Color(hex: 0x1e1e1e))
+                }
+            }
+        }
+        .padding()
+        .frame(width: 300)
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundStyle(Color(hex: 0xD1CEC7))
+        )
+    }
+}
+
 #Preview {
     HomeView()
 }
