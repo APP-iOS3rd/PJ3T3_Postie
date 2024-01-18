8442843d851a67e2418aab4e0fbf90ad176ea5d3
burst-cannon <qlrmr111@naver.com>
2024-01-18 17:33:57 +0900

    Feat: 홈뷰 구성

--- PJ3T3_Postie/Core/Home/View/HomeView.swift
+++ PJ3T3_Postie/Core/Home/View/HomeView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            NavigationStack {
+                ZStack(alignment: .bottomTrailing) {
+                    ScrollView {
+                        VStack {
+                            
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
