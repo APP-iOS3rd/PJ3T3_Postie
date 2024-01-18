c60a2d89893182ae6121f1705280a639448a94ab
Eunice <jes.e.0927@gmail.com>
2024-01-18 14:32:57 +0900

    Feat: Setting뷰에 Firestore의 user 데이터가 보여지도록 한다 - AuthViewModel을 환경변수로 받아옴 - AuthViewModel안의 currentUser가 있다면 그 유저에 해당하는 유저 정보를 받아온다.

--- PJ3T3_Postie/Core/Setting/View/SettingView.swift
+++ PJ3T3_Postie/Core/Setting/View/SettingView.swift
@@ -11,58 +11,62 @@ struct SettingView: View {
     //Colors
     private let profileBackgroundColor: Color = .gray
     private let signOutIconColor: Color = Color(uiColor: .lightGray)
+    //ViewModels
+    @ObservedObject var authViewModel = AuthViewModel.shared
     
     var body: some View {
-        List {
-            Section {
-                HStack {
-                    Text(User.MOCK_USER.initials)
-                        .font(.title)
-                        .fontWeight(.semibold)
-                        .foregroundStyle(.white)
-                        .frame(width: 72, height: 72)
-                        .background(profileBackgroundColor)
-                        .clipShape(Circle())
-                    
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text(User.MOCK_USER.fullName)
-                            .font(.subheadline)
+        if let user = authViewModel.currentUser {
+            List {
+                Section {
+                    HStack {
+                        Text(user.initials)
+                            .font(.title)
                             .fontWeight(.semibold)
-                            .padding(.top, 4)
+                            .foregroundStyle(.white)
+                            .frame(width: 72, height: 72)
+                            .background(profileBackgroundColor)
+                            .clipShape(Circle())
                         
-                        Text(User.MOCK_USER.email)
-                            .font(.footnote)
-                            .foregroundStyle(profileBackgroundColor)
-                    } //VStack
-                } //HStack
-            } //Section
-            
-            Section("General") {
-                HStack {
-                    SettingsRowView(imageName: "gear", title: "Version", tintColor: profileBackgroundColor)
-                    
-                    Spacer()
-                    
-                    Text("1.0.0")
-                        .font(.subheadline)
-                        .foregroundStyle(profileBackgroundColor)
-                }
-            } //Section
-            
-            Section("Account") {
-                Button {
-                    print("Sign out...")
-                } label: {
-                    SettingsRowView(imageName: "arrow.left.circle.fill", title: "Sign Out", tintColor: signOutIconColor)
-                }
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text(user.fullName)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.top, 4)
+                            
+                            Text(user.email)
+                                .font(.footnote)
+                                .foregroundStyle(profileBackgroundColor)
+                        } //VStack
+                    } //HStack
+                } //Section
                 
-                Button {
-                    print("Delete account")
-                } label: {
-                    SettingsRowView(imageName: "xmark.circle.fill", title: "Delete Account", tintColor: signOutIconColor)
-                }
-            } //Section
-        } //List
+                Section("General") {
+                    HStack {
+                        SettingsRowView(imageName: "gear", title: "Version", tintColor: profileBackgroundColor)
+                        
+                        Spacer()
+                        
+                        Text("1.0.0")
+                            .font(.subheadline)
+                            .foregroundStyle(profileBackgroundColor)
+                    }
+                } //Section
+                
+                Section("Account") {
+                    Button {
+                        print("Sign out...")
+                    } label: {
+                        SettingsRowView(imageName: "arrow.left.circle.fill", title: "Sign Out", tintColor: signOutIconColor)
+                    }
+                    
+                    Button {
+                        print("Delete account")
+                    } label: {
+                        SettingsRowView(imageName: "xmark.circle.fill", title: "Delete Account", tintColor: signOutIconColor)
+                    }
+                } //Section
+            } //List
+        } //if
     }
 }
 
