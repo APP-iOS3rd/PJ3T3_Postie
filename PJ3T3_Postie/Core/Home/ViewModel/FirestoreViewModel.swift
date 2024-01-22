06250e81db1bc90b9515eadb72f73fcc328c6d95
Eunice <jes.e.0927@gmail.com>
2024-01-22 11:46:03 +0900

    Feat: FirestoreViewModel을 싱글톤으로 변경

--- PJ3T3_Postie/Core/Home/ViewModel/FirestoreViewModel.swift
+++ PJ3T3_Postie/Core/Home/ViewModel/FirestoreViewModel.swift
@@ -10,19 +10,19 @@ import FirebaseAuth
 import FirebaseFirestore
 
 class FirestoreViewModel: ObservableObject {
-    @Published var letters: [Letter] = []
-
-    //로그인된 유저가 있는지 확인해서 firebase에서 제공하는 userUid를 가지고온다.
+    static let shared = FirestoreViewModel()
+    var colRef = Firestore.firestore().collection("users") //user 컬렉션 전체를 가져온다.
     var userUid: String {
+        //로그인된 유저가 있는지 확인해서 firebase에서 제공하는 userUid를 가지고온다.
         if let uid = Auth.auth().currentUser?.uid {
             return uid
         }
 
         return ""
     }
-
-    //user 컬렉션 전체를 가져온다.
-    var colRef = Firestore.firestore().collection("users")
+    @Published var letters: [Letter] = []
+    
+    private init() { }
 
     //새로운 편지를 추가한다.
     func addLetter(writer: String, recipient: String, summary: String, date: Date) {
