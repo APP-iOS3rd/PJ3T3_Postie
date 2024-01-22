faadfd3b4226f6d1f8ae3c709c93229659ed7cbb
hjsupernova <kem08120506@gmail.com>
2024-01-22 11:10:23 +0900

    Feat: LetterDetailView 인풋 값 Letter 구조체로 변경

--- PJ3T3_Postie/Core/Home/View/LetterDetailView.swift
+++ PJ3T3_Postie/Core/Home/View/LetterDetailView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct LetterDetailView: View {
     var controllers: [UIHostingController<Page>]
 
-    // TODO: 인풋 값, 편지 구조체로 변경 필요
-    init(text: String) {
-        controllers = text.chunks(size: 300).map { chunk in
-            UIHostingController(rootView: Page(text: chunk))
+    init(letter: Letter) {
+        controllers = letter.text.chunks(size: 400).map { chunk in
+            UIHostingController(rootView: Page(letter: letter, chunk: chunk))
         }
     }
 
@@ -24,7 +23,6 @@ struct LetterDetailView: View {
             
             VStack {
                 PageViewController(controllers: controllers)
-                    .frame(height: 600)
                     .clipShape(RoundedRectangle(cornerRadius: 10.0))
             }
             .padding()
@@ -45,11 +43,6 @@ struct LetterDetailView: View {
 
 #Preview {
     NavigationStack {
-        LetterDetailView(text: Constants.textSample)
+        LetterDetailView(letter: Letter.preview)
     }
-}
-
-// 샘플 데이터입니다.
-struct Constants {
-    static let textSample = "안녕? 잘지내? 한동안 따뜻 했다가 다시 추워졌네. 항상 감기 조심해... 이런 추운 겨울만 되면 항상 너가 생각나. 추운 겨울이 생각나지 않을 정도로 우리, 따뜻했잖아. 그런 겨울이 다시 올줄 알았는데 안타깝게도 그렇게 되진 못헀네. 너랑 헤어진 이후 내 머리속엔 항상 너로 가득했어. 돌이킬 수 없는 나의 실수에 자책을 너무 했어. 잊어보려 노력했지만, 시릴 정도로 추운 겨울이 너와 같이 찾아왔어. 처음이였어. 너도 처음이였고 이렇게 무언가에 빠진 나도 처음이야. 우리 다시 만나면 그 전 보다 더 잘될 수 있지 않을까...? 안녕? 잘지내? 한동안 따뜻 했다가 다시 추워졌네. 항상 감기 조심해... 이런 추운 겨울만 되면 항상 너가 생각나. 추운 겨울이 생각나지 않을 정도로 우리, 따뜻했잖아. 그런 겨울이 다시 올줄 알았는데 안타깝게도 그렇게 되진 못헀네. 너랑 헤어진 이후 내 머리속엔 항상 너로 가득했어. 돌이킬 수 없는 나의 실수에 자책을 너무 했어. 잊어보려 노력했지만, 시릴 정도로 추운 겨울이 너와 같이 찾아왔어. 처음이였어. 너도 처음이였고 이렇게 무언가에 빠진 나도 처음이야. 우리 다시 만나면 그 전 보다 더 잘될 수 있지 않을까...? 안녕? 잘지내? 한동안 따뜻 했다가 다시 추워졌네. 항상 감기 조심해... 이런 추운 겨울만 되면 항상 너가 생각나. 추운 겨울이 생각나지 않을 정도로 우리, 따뜻했잖아. 그런 겨울이 다시 올줄 알았는데 안타깝게도 그렇게 되진 못헀네. 너랑 헤어진 이후 내 머리속엔 항상 너로 가득했어. 돌이킬 수 없는 나의 실수에 자책을 너무 했어. 잊어보려 노력했지만, 시릴 정도로 추운 겨울이 너와 같이 찾아왔어. 처음이였어. 너도 처음이였고 이렇게 무언가에 빠진 나도 처음이야. 우리 다시 만나면 그 전 보다 더 잘될 수 있지 않을까...? "
 }
--- PJ3T3_Postie/Core/Home/View/PageViewController.swift
+++ PJ3T3_Postie/Core/Home/View/PageViewController.swift
@@ -41,31 +41,31 @@ struct PageViewController: UIViewControllerRepresentable {
         func pageViewController(
             _ pageViewController: UIPageViewController,
             viewControllerBefore viewController: UIViewController) -> UIViewController? {
-            guard let index = self.parent.controllers.firstIndex(of: viewController) else {
-                return nil
-            }
+                guard let index = self.parent.controllers.firstIndex(of: viewController) else {
+                    return nil
+                }
 
-            if index == 0 {
-                return self.parent.controllers.last
-            }
+                if index == 0 {
+                    return self.parent.controllers.last
+                }
 
-            return self.parent.controllers[index - 1]
-        }
+                return self.parent.controllers[index - 1]
+            }
 
         // 오른쪽 스와이프 하면 나올 뷰 설정
         func pageViewController(
             _ pageViewController: UIPageViewController,
             viewControllerAfter viewController: UIViewController) -> UIViewController? {
-            guard let index = self.parent.controllers.firstIndex(of: viewController) else {
-                return nil
-            }
+                guard let index = self.parent.controllers.firstIndex(of: viewController) else {
+                    return nil
+                }
 
-            if index == self.parent.controllers.count - 1 {
-                return self.parent.controllers.first
-            }
+                if index == self.parent.controllers.count - 1 {
+                    return self.parent.controllers.first
+                }
 
-            return self.parent.controllers[index + 1 ]
-        }
+                return self.parent.controllers[index + 1 ]
+            }
 
     }
 
@@ -75,43 +75,48 @@ struct PageViewController: UIViewControllerRepresentable {
 }
 
 struct Page: View {
-    var text: String
+    let letter: Letter
+    var chunk: String
 
     var body: some View {
         VStack(spacing: 16) {
-            HStack{
-                Text("To. 국연수")
+            HStack {
+                Text("To. \(letter.receiver)")
                     .font(.headline)
+
                 Spacer()
             }
 
             Divider()
 
-            VStack(spacing: 16) {
-                HStack {
-                    Image(systemName: "quote.opening")
-                        .font(.title)
-                        .foregroundStyle(Color(hex: 0xC2AD7E))
-                    Spacer()
-                }
+            HStack {
+                Image(systemName: "quote.opening")
+                    .font(.title)
+                    .foregroundStyle(Color(hex: 0xC2AD7E))
 
-
-                Text(text)
-                    .lineSpacing(10.0)
-
-                HStack {
-                    Spacer()
-                    Image(systemName: "quote.closing")
-                        .font(.title)
-                        .foregroundStyle(Color(hex: 0xC2AD7E))
-                }
+                Spacer()
             }
-            .padding(.vertical, 28)
+            .padding(.bottom, 8)
+
+            Text(chunk)
+                .lineSpacing(10.0)
+
+            Spacer()
+
+            HStack {
+                Spacer()
+
+                Image(systemName: "quote.closing")
+                    .font(.title)
+                    .foregroundStyle(Color(hex: 0xC2AD7E))
+            }
 
             HStack {
                 Text(Date.now.formatted().description)
+
                 Spacer()
-                Text("From. 최웅")
+
+                Text("From. \(letter.sender)")
                     .font(.headline)
             }
         }
@@ -119,6 +124,10 @@ struct Page: View {
     }
 }
 
+//#Preview {
+//    Page(letter: Letter.preview, chunk: "안녕? 잘지내? 한동안 따뜻 했다가 다시 추워졌네. 항상 감기 조심해... 이런 추운 겨울만 되면 항상 너가 생각나. 추운 겨울이 생각나지 않을 정도로 우리, 따뜻했잖아. 그런 겨울이 다시 올줄 알았는데 안타깝게도 그렇게 되진 못헀네. 너랑 헤어진 이후 내 머리속엔 항상 너로 가득했어. 돌이킬 수 없는 나의 실수에 자책을 너무 했어. 잊어보려 노력했지만, 시릴 정도로 추운 겨울이 너와 같이 찾아왔어. 처음이였어. 너도 처음이였고 이렇게 무언가에 빠진 나도 처음이야. 우리 다시 만나면 그 전 보다 더 잘될 수 있지 않을까...?")
+//}
+
 #Preview {
-    Page(text: "안녕? 잘지내? 한동안 따뜻 했다가 다시 추워졌네. 항상 감기 조심해... 이런 추운 겨울만 되면 항상 너가 생각나. 추운 겨울이 생각나지 않을 정도로 우리, 따뜻했잖아. 그런 겨울이 다시 올줄 알았는데 안타깝게도 그렇게 되진 못헀네. 너랑 헤어진 이후 내 머리속엔 항상 너로 가득했어. 돌이킬 수 없는 나의 실수에 자책을 너무 했어. 잊어보려 노력했지만, 시릴 정도로 추운 겨울이 너와 같이 찾아왔어. 처음이였어. 너도 처음이였고 이렇게 무언가에 빠진 나도 처음이야. 우리 다시 만나면 그 전 보다 더 잘될 수 있지 않을까...?")
+    LetterDetailView(letter: Letter.preview)
 }
