da8df6b7a318e7191ec06555ef0a511f93eb7555
Eunice <jes.e.0927@gmail.com>
2024-01-22 14:52:23 +0900

    Remove: 불필요한 모델 extension 삭제

--- PJ3T3_Postie/Model/Letter.swift
+++ PJ3T3_Postie/Model/Letter.swift
@@ -15,7 +15,3 @@ struct Letter: Codable, Hashable, Identifiable {
     var summary: String
     var date: Date
 }
-
-extension Letter {
-    static var letterSampleData = Letter(id: UUID().uuidString, writer: "Eunice", recipient: "Tosim", summary: "토심토심아 토뭉이는 어딨어?", date: Date())
-}
