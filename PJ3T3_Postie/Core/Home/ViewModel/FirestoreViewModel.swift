26922fd198fb2289670ddce2c8968ed497355fcf
Eunice <jes.e.0927@gmail.com>
2024-01-22 11:40:51 +0900

    Feat: 새로운 letter 데이터를 추가할 수 있다 - LetterListView의 Button을 누르면 미리 설정된 데이터가 현재 로그인해 있는 유저의 문서 아래의 collection인 letters에 새 문서로 저장된다.

--- PJ3T3_Postie/Core/Home/ViewModel/FirestoreViewModel.swift
+++ PJ3T3_Postie/Core/Home/ViewModel/FirestoreViewModel.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+
+class FirestoreViewModel: ObservableObject {
+    @Published var letters: [Letter] = []
+
+    //로그인된 유저가 있는지 확인해서 firebase에서 제공하는 userUid를 가지고온다.
+    var userUid: String {
+        if let uid = Auth.auth().currentUser?.uid {
+            return uid
+        }
+
+        return ""
+    }
+
+    //user 컬렉션 전체를 가져온다.
+    var colRef = Firestore.firestore().collection("users")
+
+    //새로운 편지를 추가한다.
+    func addLetter(writer: String, recipient: String, summary: String, date: Date) {
+        let document = colRef.document(userUid).collection("letters").document() //새로운 document를 생성한다.
+        let documentId = document.documentID //생성한 document의 id를 가져온다.
+
+        //Letter model에 맞는 모양으로 document data를 생성한다.
+        let docData: [String: Any] = [
+            "id": documentId,
+            "writer": writer,
+            "recipient": recipient,
+            "summary": summary,
+            "date": date,
+        ]
+
+        //생성한 데이터를 해당되는 경로에 새롭게 생성한다. 이때 overwrite 하지 않는다.
+        document.setData(docData, merge: false) { error in
+            if let error = error {
+                print(error)
+            } else {
+                print("Success:", documentId)
+            }
+        }
+    }
+}
--- PJ3T3_Postie/Model/Letter.swift
+++ PJ3T3_Postie/Model/Letter.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+struct Letter: Codable, Hashable, Identifiable {
+    var id: String
+    var writer: String
+    var recipient: String
+    var summary: String
+    var date: Date
+}
+
+extension Letter {
+    static var letterSampleData = Letter(id: UUID().uuidString, writer: "Eunice", recipient: "Tosim", summary: "토심토심아 토뭉이는 어딨어?", date: Date())
+}
