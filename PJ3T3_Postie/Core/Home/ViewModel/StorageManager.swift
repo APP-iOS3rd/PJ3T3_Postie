87e8457ab0b2e43fd1aa20606bbbda191e9a5688
Eunice <jes.e.0927@gmail.com>
2024-01-22 18:44:39 +0900

    Feat: StorageManager에 Storage에 이미지 업로드하는 함수 생성

--- PJ3T3_Postie/Core/Home/ViewModel/StorageManager.swift
+++ PJ3T3_Postie/Core/Home/ViewModel/StorageManager.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import FirebaseStorage
+
+final class StorageManager {
+    static let shared = StorageManager()
+    
+    private init() { }
+    
+    private let storage = Storage.storage().reference() //db의 root reference location
+    
+    func saveImage(data: Data) async throws -> (path: String, name: String) {
+        //metadata없이도 data를 업로드 할 수 있지만, 그 경우 서버는 어떤 타입의 데이터를 저장하는지 알지 못해 오류를 발생시킬 수 있으므로 upload하는 metadata 타입을 명시 해 주는 편이 좋다.
+        let meta = StorageMetadata()
+        meta.contentType = "image/jpeg"
+        
+        let path = "\(UUID().uuidString).jpeg"
+        let returnedMetaData = try await storage.child(path).putDataAsync(data, metadata: meta)
+        
+        guard let returnedPath = returnedMetaData.path, let returnedName = returnedMetaData.name else {
+            throw URLError(.badServerResponse)
+        }
+        
+        return (returnedPath, returnedName)
+    }
+}
