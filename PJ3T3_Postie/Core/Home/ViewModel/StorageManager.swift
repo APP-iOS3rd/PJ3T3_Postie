dd61fa74a7d14b3a95d095937f4167626304edf0
Eunice <jes.e.0927@gmail.com>
2024-01-23 21:32:21 +0900

    Feat: UIImage타입을 가지는 배열을 받아 Storage저장하는 함수 구현
    - [UIImage]를 받아 반복문을 돌며 각 이미지들을 Data type으로 변환
    - saveImage 함수를 호출해 Storage에 이미지를 업로드하고
    - 업로드가 완료될 때 마다 업로드된 이미지의 urlString을 urlArray에 append한다.

--- PJ3T3_Postie/Core/Home/ViewModel/StorageManager.swift
+++ PJ3T3_Postie/Core/Home/ViewModel/StorageManager.swift
@@ -53,15 +53,38 @@ final class StorageManager: ObservableObject {
         return nil
     }
     
-    //UIImage 타입을 받아 저장할 수 있다. 사용하지 않아 삭제 할 경우 UIKit import도 함께 삭제한다.
-    func saveImage(image: UIImage, userId: String) async throws -> (path: String, name: String) {
-        //compressionQuality: 1 => 100%를 의미해 압축 없음
-        //이미지가 너무 클 경우 직접 compress하거나 firebase extension 중 resize images(유료)를 사용해보자.
-        //이미지 타입이 png라면 data = image.png()
-        guard let data = image.jpegData(compressionQuality: 1) else {
-            throw URLError(.backgroundSessionWasDisconnected)
+    /**
+     [UIImage]를 받아 반복문을 돌며 각 이미지들을 Data type으로 변환, saveImage 함수를 호출해 Storage에 이미지를 업로드하고
+     업로드가 완료될 때 마다 업로드된 이미지의 urlString을 urlArray에 append한다.
+     ### Notes ###
+     - _이미지 업로드 실패 alert창 띄워주기_
+     - Parameters:
+       - images: Storage에 저장하고싶은 이미지들의 배열
+       - userId: 현재 로그인중인 유저의 uuid로 이미지를 업로드 할 경로를 생성하거나 찾는데 사용한다.
+     - Returns: [String]: 업로드 완료 된 이미지의 urlString으로 구성된 배열
+     */
+    func saveUIImage(images: [UIImage], userId: String) async throws -> [String] {
+        var urlArray: [String] = []
+        
+        for img in images {
+            print("업로드 시작")
+            
+            //compressionQuality: 1 => 100%를 의미해 압축 없음
+            //이미지가 너무 클 경우 직접 compress하거나 firebase extension 중 resize images(유료)를 사용
+            //이미지 타입이 png라면 data = image.png()
+            guard let data = img.jpegData(compressionQuality: 1) else {
+                throw URLError(.backgroundSessionWasDisconnected)
+            }
+            
+            guard let url = try await saveImage(data: data, userId: userId) else {
+                //이미지 업로드 실패 alert창 띄워주기
+                return []
+            }
+            
+            urlArray.append(url)
         }
         
-        return try await saveImage(data: data, userId: userId)
+        print("업로드 종료")
+        return urlArray
     }
 }
