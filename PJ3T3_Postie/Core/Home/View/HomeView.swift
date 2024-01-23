625160e17907d51a5a5582f7d3b62bd3ef5056f3
burst-cannon <qlrmr111@naver.com>
2024-01-23 15:47:18 +0900

    Design: 편지지 UI 변경

--- PJ3T3_Postie/Core/Home/View/HomeView.swift
+++ PJ3T3_Postie/Core/Home/View/HomeView.swift
@@ -18,11 +18,15 @@ struct HomeView: View {
                     ScrollView {
                         VStack {
                             NavigationLink(destination: LetterDetailView(letter: Letter.preview)) {
-                                sendLetterView(sender: "김OO", date: "2024.01.04", receiver: "어피치")
+                                receiveLetterView(sender: "최웅", date: "2024.01.08", summary: "이런 추운 겨울만 되면 항상 너가 생각나")
                             }
                             
                             NavigationLink(destination: LetterDetailView(letter: Letter.preview)) {
-                                receiveLetterView(sender: "라이언", date: "2024.01.04", receiver: "김OO")
+                                sendLetterView(sender: "최웅", date: "2024.01.09", summary: "넌 뭔데 그렇게 아무렇지 않게 구는건데?")
+                            }
+                            
+                            NavigationLink(destination: LetterDetailView(letter: Letter.preview)) {
+                                receiveLetterView(sender: "할머니", date: "2024.01.08", summary: "나 때문에 살지마, 연수야")
                             }
                         }
                         .padding()
@@ -38,10 +42,19 @@ struct HomeView: View {
                                 .foregroundStyle(Color(hex: 0x979797))
                                 .frame(width:70,height:70)
                             
-                            Image(systemName: "envelope")
+                            ZStack {
+                                Image(systemName: "envelope")
+                                    .font(.title2)
+                                    .offset(y: -5)
+                                
+                                Image(systemName: "plus.circle")
+                                    .font(.footnote)
+                                    .offset(x:15, y:9)
+                            }
                         }
                     })
                     .foregroundStyle(Color(hex: 0x1E1E1E))
+                    .shadow(color: Color.black.opacity(0.1), radius: 3, x: 3, y: 3)
                     .imageScale(.large)
                     .padding()
                     .alert("편지 저장 하기", isPresented: $showAlert) {
@@ -61,115 +74,94 @@ struct HomeView: View {
     }
 }
 
-func receiveLetterView(sender: String, date: String, receiver: String) -> some View {
+func receiveLetterView(sender: String, date: String, summary: String) -> some View {
     HStack {
         HStack {
             VStack(alignment: .leading) {
                 HStack {
-                    RoundedRectangle(cornerRadius: 10)
-                        .frame(width: 100, height: 35)
-                        .overlay(
-                            Text("보내는 사람")
-                                .foregroundStyle(Color(hex: 0x1E1E1E))
-                        )
-                        .foregroundStyle(Color(hex: 0x979797))
+                    Text("From.")
+                        .font(.custom("SourceSerifPro-Black", size: 18))
+                        .foregroundColor(.black)
                     
                     Text("\(sender)")
                         .foregroundStyle(Color(hex: 0x1E1E1E))
                     
                     Spacer()
                     
-                    Image(systemName: "swift")
-                        .font(.title)
+                    Text(date)
+                        .font(.custom("SourceSerifPro-Light", size: 18))
                         .foregroundStyle(Color(hex: 0x1E1E1E))
+                    
+                    ZStack {
+                        Image(systemName: "water.waves")
+                            .font(.headline)
+                            .offset(x:18)
+                        Image(systemName: "sleep.circle")
+                            .font(.largeTitle)
+                    }
+                    .foregroundStyle(Color.init(hex: 0x979797))
                 }
                 
-                Text(" ")
+                Text("")
                 
-                HStack {
-                    VStack (alignment: .leading) {
-                        Text(date)
-                            .foregroundStyle(Color(hex: 0x1E1E1E))
-                    }
-                    
-                    Spacer()
-                    
-                    RoundedRectangle(cornerRadius: 10)
-                        .frame(width: 90, height: 35)
-                        .overlay(
-                            Text("받는 사람")
-                                .foregroundColor(.black)
-                        )
-                        .foregroundStyle(Color(hex: 0x979797))
-                    
-                    Text("\(receiver)")
-                        .foregroundStyle(Color(hex: 0x1E1E1E))
+                if summary != "" {
+                    Text("\"\(summary)\"")
                 }
             }
         }
         .padding()
-        .frame(width: 300)
+        .frame(width: 300, height: 130)
         .background(
             RoundedRectangle(cornerRadius: 10)
-                .foregroundStyle(Color(hex: 0xD1CEC7))
+                .foregroundStyle(Color(hex: 0xD1CEC7).opacity(0.65))
+                .shadow(color: Color.black.opacity(0.1), radius: 3, x: 3, y: 3)
         )
         
         Spacer()
     }
 }
 
-func sendLetterView(sender: String, date: String, receiver: String) -> some View {
+func sendLetterView(sender: String, date: String, summary: String) -> some View {
     HStack {
         Spacer()
         
         HStack {
             VStack(alignment: .leading) {
                 HStack {
-                    RoundedRectangle(cornerRadius: 10)
-                        .frame(width: 100, height: 35)
-                        .overlay(
-                            Text("보내는 사람")
-                                .foregroundColor(.black)
-                        )
-                        .foregroundStyle(Color(hex: 0x979797))
+                    Text("To.")
+                        .font(.custom("SourceSerifPro-Black", size: 18))
+                        .foregroundColor(.black)
                     
                     Text("\(sender)")
                         .foregroundStyle(Color(hex: 0x1E1E1E))
                     
                     Spacer()
                     
-                    Image(systemName: "swift")
+                    Text(date)
+                        .font(.custom("SourceSerifPro-Light", size: 18))
                         .foregroundStyle(Color(hex: 0x1E1E1E))
-                        .font(.title)
+                    
+                    ZStack {
+                        Image(systemName: "water.waves")
+                            .font(.headline)
+                            .offset(x:18)
+                        Image(systemName: "sleep.circle")
+                            .font(.largeTitle)
+                    }
+                    .foregroundStyle(Color.init(hex: 0x979797))
                 }
                 
-                Text(" ")
-                
-                HStack {
-                    VStack (alignment: .leading) {
-                        Text(date)
-                    }
-                    
-                    Spacer()
-                    
-                    RoundedRectangle(cornerRadius: 10)
-                        .frame(width: 90, height: 35)
-                        .overlay(
-                            Text("받는 사람")
-                                .foregroundColor(Color(hex: 0x1E1E1E))
-                        )
-                        .foregroundStyle(Color(hex: 0x979797))
-                    
-                    Text("\(receiver)")
-                        .foregroundStyle(Color(hex: 0x1E1E1E))
+                if summary != "" {
+                    Text("\"\(summary)\"")
                 }
             }
         }
         .padding()
-        .frame(width: 300)
+        .frame(width: 300, height: 130)
         .background(
             RoundedRectangle(cornerRadius: 10)
-                .foregroundStyle(Color(hex: 0xD1CEC7))
+                .foregroundStyle(Color(hex: 0xF7F7F7).opacity(0.65))
+                .shadow(color: Color.black.opacity(0.1), radius: 3, x: 3, y: 3)
         )
     }
 }
