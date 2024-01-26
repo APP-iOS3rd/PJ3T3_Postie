af3dce1c949be60337f2870aea0ea7b53059e969
Eunice <jes.e.0927@gmail.com>
2024-01-26 19:57:31 +0900

    Feat: LetterPhoto구조체와 파일을 생성하고 StorageManager에서 LetterPhoto형식의 배열을 publish

--- PJ3T3_Postie/Core/Home/ViewModel/StorageManager.swift
+++ PJ3T3_Postie/Core/Home/ViewModel/StorageManager.swift
@@ -11,6 +11,7 @@ import UIKit
 
 final class StorageManager: ObservableObject {
     static let shared = StorageManager()
+    @Published var images: [LetterPhoto] = []
     
     private init() { }
     
