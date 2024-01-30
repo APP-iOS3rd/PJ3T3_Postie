80904223a540ff30dd0222e48f2fe55f4ccc56d3
Eunice <jes.e.0927@gmail.com>
2024-01-30 15:46:55 +0900

    Refactor: AuthManager 클래스 전체가 MainActor에서 작동하지 않도록 수정
    일부 main 스레드에서 작동해야 하는 코드만 dispatchQueue로 작동하도록 수정했다.

--- PJ3T3_Postie/Manager/AuthManager.swift
+++ PJ3T3_Postie/Manager/AuthManager.swift
@@ -13,13 +13,6 @@ protocol AuthenticationProtocol {
     var formIsValid: Bool { get }
 }
 
-//UI 업데이트는 꼭 메인 스레드에서 진행되어야 한다.
-//비동기 네트워킹은 기본적으로 메인이 아닌 다른 스레드에서 진행되므로
-//UI 업데이트를 하는 Publish가 만드시 메인 스레드에서 수행되도록 설정하기 위해 @MainActor를 선언해 주는 것이다.
-
-//Actor 내에서 구현이 실행중인 모든 작업은 항상 메인 큐에서 수행하게 된다.
-//Task 로 생성된 작업은 (메인 액터에서 생성되지 않는 한) 백그라운드 스레드에서 즉시 실행되며, await 키워드를 사용해서 완료된 값이 돌아올 때까지 기다릴 수 있다.
-@MainActor
 class AuthManager: ObservableObject {
     static let shared = AuthManager()
     @Published var userSession: FirebaseAuth.User? //Firebase user object
@@ -38,7 +31,11 @@ class AuthManager: ObservableObject {
     func signIn(withEamil email: String, password: String) async throws {
         do {
             let result = try await Auth.auth().signIn(withEmail: email, password: password)
-            self.userSession = result.user
+            
+            DispatchQueue.main.async {
+                self.userSession = result.user
+            }
+            
             //fetchUser가 uid로 firebase에서 데이터를 찾기 위해서는 반드시 signIn이 완료 된 다음 fetchUser 함수를 호출해야 한다.
             await fetchUser()
         } catch {
@@ -52,7 +49,9 @@ class AuthManager: ObservableObject {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
 
             //2. Once success, set userSession property
-            self.userSession = result.user //result에서 user값을 받아와 userSession에 넣어줌
+            DispatchQueue.main.async {
+                self.userSession = result.user //result에서 user값을 받아와 userSession에 넣어줌
+            }
 
             //3. Create our User object
             //firebase의 user는 firebase에서 새로운 user를 생성하고 uid 를 제공해준다.
@@ -93,8 +92,14 @@ class AuthManager: ObservableObject {
         //현재 로그인중인 유저가 있다면 fetch가 진행된다. 로그인 유저가 없다면 해당 guard문을 통과하지 못하고 return된다.
         guard let uid = Auth.auth().currentUser?.uid else { return }
         guard let snapshot = try? await Firestore.firestore().collection("users").document(uid).getDocument() else { return }
-        //Firestore에서 받은 데이터를 User model에 맞는 형태로 변환하여 currentUser에 값을 부여한다.
-        self.currentUser = try? snapshot.data(as: User.self)
-        print("DEBUG: Current user is \(self.currentUser)")
+        
+        //UI 업데이트는 꼭 메인 스레드에서 진행되어야 하는데
+        //비동기 네트워킹은 기본적으로 메인이 아닌 다른 스레드에서 진행되므로 UI 업데이트를 하는 Publish가 반드시 메인 스레드에서 수행되도록 설정하기 위해
+        //클래스 상단에 @MainActor를 선언해 주거나 아래와 같이 DispatchQueue.main.async 에서 코드를 실행해야 한다.
+        DispatchQueue.main.async {
+            //Firestore에서 받은 데이터를 User model에 맞는 형태로 변환하여 currentUser에 값을 부여한다.
+            self.currentUser = try? snapshot.data(as: User.self)
+            print("DEBUG: Current user is \(self.currentUser)")
+        }
     }
 }
