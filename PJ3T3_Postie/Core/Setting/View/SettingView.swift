7b313405ed8591cbc0bcaf0c2948f8a3dbd03fd3
Eunice <jes.e.0927@gmail.com>
2024-01-30 20:34:57 +0900

    Remove: 사용하지 않는 ObservedObject 관련 코드 삭제

--- PJ3T3_Postie/Core/Setting/View/SettingView.swift
+++ PJ3T3_Postie/Core/Setting/View/SettingView.swift
@@ -10,7 +10,6 @@ import PhotosUI //Storage test를 위한 import로 이후 삭제 예정
 
 struct SettingView: View {
     @ObservedObject var authViewModel = AuthManager.shared
-    @ObservedObject var storageManager = StorageManager.shared
     //Colors
     private let profileBackgroundColor: Color = .gray
     private let signOutIconColor: Color = Color(uiColor: .lightGray)
