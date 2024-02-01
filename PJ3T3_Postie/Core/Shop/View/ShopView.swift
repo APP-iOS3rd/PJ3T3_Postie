41286335b4a4c390b83770c4d2e3066a1992df86
Im-loki <al4126@naver.com>
2024-02-01 11:18:41 +0900

    Style: 코드 컨벤션에 맞게 수정

--- PJ3T3_Postie/Core/Shop/View/ShopView.swift
+++ PJ3T3_Postie/Core/Shop/View/ShopView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 import SafariServices
 
 struct ShopView: View {
-    
-    @State private var selectedButtonIndex: Int = 0
-    @State private var showDetails = false
-    @State private var safariURL: String?
-    
+    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
+    let dummyData = [["poketmon2", "sanrio", "digimon"],["kuma", "crayon", "poketmon"], [], [], []]
+    let postDivision: [String] = ["캐릭터", "심플", "일러스트", "풍경", "컬러풀"]
+    let postImage0: [String] = ["poketmon", "poketmon2", "sanrio", "digimon", "kuma", "crayon"]
+    let postImage1: [String] = ["poketmon2", "sanrio", "digimon", "kuma", "crayon", "poketmon"]
     let urls: [String: String] = [
         "poketmon" : "https://gloomy.co.kr/product/%ED%8F%AC%EC%BC%93%EB%AA%AC%EC%8A%A4%ED%84%B0-%EC%BA%90%EB%A6%AD%ED%84%B0-%ED%8E%B8%EC%A7%80%EC%A7%80-4p%EC%84%B8%ED%8A%B8%EB%9E%9C%EB%8D%A4%EB%B0%9C%EC%86%A1-129693/101233/",
         "poketmon2" :
@@ -26,11 +26,10 @@ struct ShopView: View {
         "kuma" : "https://akaikaze00.cafe24.com/product/%EC%9D%BC%EB%B3%B8-%EB%A6%AC%EB%9D%BD%EC%BF%A0%EB%A7%88-%ED%8E%B8%EC%A7%80%EC%A7%80-%ED%8E%B8%EC%A7%80%EB%B4%89%ED%88%AC%EC%84%B8%ED%8A%B8%EA%B3%BC%EC%9D%BC/17384/",
         "crayon" : "https://akaikaze00.cafe24.com/product/%EC%A7%B1%EA%B5%AC%EB%8A%94%EB%AA%BB%EB%A7%90%EB%A0%A4-%ED%8E%B8%EC%84%A0%EC%A7%80-%ED%8E%B8%EC%A7%80%EC%A7%80%EC%84%B8%ED%8A%B8%EC%8B%9C%EC%A6%8C6-4color/18003/"
     ]
-    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
-    let postDivision: [String] = ["캐릭터", "심플", "일러스트", "풍경", "컬러풀"]
-    let postImage0: [String] = ["poketmon", "poketmon2", "sanrio", "digimon", "kuma", "crayon"]
-    let postImage1: [String] = ["poketmon2", "sanrio", "digimon", "kuma", "crayon", "poketmon"]
-    let dummyData = [["poketmon2", "sanrio", "digimon"],["kuma", "crayon", "poketmon"], [], [], []]
+    
+    @State private var safariURL: String?
+    @State private var selectedButtonIndex: Int = 0
+    @State private var showDetails = false
     
     var body: some View {
         NavigationStack {
@@ -92,14 +91,14 @@ struct ShopView: View {
                             }) {
                                 Image(character)
                                     .resizable()
-                                    .scaledToFit()//이미지 비율 조정
+                                    .scaledToFit() //이미지 비율 조정
                                     .frame(width: 157, height: 180)
                                     .padding()
                             }
                             .sheet(item: $safariURL) { url in
                                 if let url = URL(string: url) {
                                     SafariView(url: url)
-                                        .ignoresSafeArea()//외부링크가 화면에 맞게 조절
+                                        .ignoresSafeArea() //외부링크가 화면에 맞게 조절
                                 }
                             }
                         }
@@ -116,7 +115,6 @@ extension String: Identifiable {
 }
 
 struct SafariView: UIViewControllerRepresentable {
-    
     let url: URL
     
     func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
@@ -126,7 +124,6 @@ struct SafariView: UIViewControllerRepresentable {
     func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
         
     }
-    
 }
 
 #Preview {
