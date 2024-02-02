b1f6b332371ad8b5b5057eced4979089cc034255
Eunice <jes.e.0927@gmail.com>
2024-02-02 19:55:28 +0900

    Feat: hasAccount 변수가 publish되지 않도록 수정

--- PJ3T3_Postie/Manager/AuthManager.swift
+++ PJ3T3_Postie/Manager/AuthManager.swift
@@ -20,11 +20,11 @@ enum AuthProviderOption: String {
 
 class AuthManager: ObservableObject {
     static let shared = AuthManager()
+    var hasAccount: Bool = true
     @Published var userUid: String  = ""
     @Published var userSession: FirebaseAuth.User? //Firebase user object
     @Published var currentUser: PostieUser? //User Data Model
     @Published var authDataResult: AuthDataResult?
-    @Published var hasAccount: Bool = true
     
     private init() {
         Task {
