6d675d9d4c17a61cd191c2226ccf43934ee06a88
burst-cannon <qlrmr111@naver.com>
2024-02-07 10:07:34 +0900

    Add: 프로필 수정 뷰 추가

---
+++ PJ3T3_Postie/Core/Setting/View/ProfileEditView.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileEditView.swift
+//  PJ3T3_Postie
+//
+//  Created by 권운기 on 2/7/24.
+//
+
+import SwiftUI
+
+struct ProfileEditView: View {
+    var body: some View {
+        ZStack {
+            Color.postieBeige
+                .ignoresSafeArea()
+            
+            Text("프로필 수정뷰")
+        }
+    }
+}
+
+#Preview {
+    ProfileEditView()
+}
