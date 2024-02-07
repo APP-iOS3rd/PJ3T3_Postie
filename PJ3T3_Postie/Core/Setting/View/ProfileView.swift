11dfc9e062d15aed78cbd95a217457613fa8f54b
burst-cannon <qlrmr111@naver.com>
2024-02-07 09:25:48 +0900

    Feat: 프로필뷰 스크롤 뷰로 변경

--- PJ3T3_Postie/Core/Setting/View/ProfileView.swift
+++ PJ3T3_Postie/Core/Setting/View/ProfileView.swift
@@ -19,72 +19,74 @@ struct ProfileView: View {
             postieColors.backGroundColor
                 .ignoresSafeArea()
             
-            VStack(alignment: .leading) {
-                HStack {
-                    Spacer()
-                    
-                    ZStack {
-                        Circle()
-                            .frame(width: 170, height: 170)
-                            .foregroundStyle(postieColors.profileColor)
+            ScrollView {
+                VStack(alignment: .leading) {
+                    HStack {
+                        Spacer()
                         
-                        Image("postyReceivingBeige")
-                            .resizable()
-                            .frame(width: 170, height: 170)
+                        ZStack {
+                            Circle()
+                                .frame(width: 170, height: 170)
+                                .foregroundStyle(postieColors.profileColor)
+                            
+                            Image("postyReceivingBeige")
+                                .resizable()
+                                .frame(width: 170, height: 170)
+                        }
+                        
+                        Spacer()
                     }
                     
-                    Spacer()
-                }
-                
-                Text("이름")
-                    .foregroundStyle(postieColors.dividerColor)
-                
-                DividerView(isThemeGroupButton: $isThemeGroupButton)
-                
-                Text("Postie_test")
-                    .foregroundStyle(postieColors.tabBarTintColor)
-                    .font(.title3)
-                    .padding(.bottom)
-                
-                Text("계정")
-                    .foregroundStyle(postieColors.dividerColor)
-                
-                DividerView(isThemeGroupButton: $isThemeGroupButton)
-                
-                Text("postie@test.com")
-                    .foregroundStyle(postieColors.tabBarTintColor)
-                    .font(.title3)
-                    .padding(.bottom)
-                
-                Text("구독정보")
-                    .foregroundStyle(postieColors.dividerColor)
-                
-                DividerView(isThemeGroupButton: $isThemeGroupButton)
-                
-                Text("일반회원")
-                    .foregroundStyle(postieColors.tabBarTintColor)
-                    .font(.title3)
-                    .padding(.bottom)
-                
-                Text("계정 관리")
-                    .foregroundStyle(postieColors.dividerColor)
-                
-                DividerView(isThemeGroupButton: $isThemeGroupButton)
-                
-                Button {
-                    authManager.signOut()
-                } label: {
-                    Text("로그아웃")
+                    Text("이름")
+                        .foregroundStyle(postieColors.dividerColor)
+                    
+                    DividerView(isThemeGroupButton: $isThemeGroupButton)
+                    
+                    Text("Postie_test")
                         .foregroundStyle(postieColors.tabBarTintColor)
                         .font(.title3)
                         .padding(.bottom)
+                    
+                    Text("계정")
+                        .foregroundStyle(postieColors.dividerColor)
+                    
+                    DividerView(isThemeGroupButton: $isThemeGroupButton)
+                    
+                    Text("postie@test.com")
+                        .foregroundStyle(postieColors.tabBarTintColor)
+                        .font(.title3)
+                        .padding(.bottom)
+                    
+                    Text("구독정보")
+                        .foregroundStyle(postieColors.dividerColor)
+                    
+                    DividerView(isThemeGroupButton: $isThemeGroupButton)
+                    
+                    Text("일반회원")
+                        .foregroundStyle(postieColors.tabBarTintColor)
+                        .font(.title3)
+                        .padding(.bottom)
+                    
+                    Text("계정 관리")
+                        .foregroundStyle(postieColors.dividerColor)
+                    
+                    DividerView(isThemeGroupButton: $isThemeGroupButton)
+                    
+                    Button {
+                        authManager.signOut()
+                    } label: {
+                        Text("로그아웃")
+                            .foregroundStyle(postieColors.tabBarTintColor)
+                            .font(.title3)
+                            .padding(.bottom)
+                    }
+                    
+                    Text("회원탈퇴")
+                        .foregroundStyle(postieColors.tabBarTintColor)
+                        .font(.title3)
+                    
+                    Spacer()
                 }
-                
-                Text("회원탈퇴")
-                    .foregroundStyle(postieColors.tabBarTintColor)
-                    .font(.title3)
-                
-                Spacer()
             }
             .tint(postieColors.tabBarTintColor)
             .padding()
