f236de887eafbb49eca5de8941836a431bb83ed1
burst-cannon <qlrmr111@naver.com>
2024-02-11 17:20:55 +0900

    Refactor: 정렬 함수 변경

--- PJ3T3_Postie/Core/Home/View/GroupedLetterView.swift
+++ PJ3T3_Postie/Core/Home/View/GroupedLetterView.swift
@@ -21,24 +21,39 @@ struct GroupedLetterView: View {
     // 숫자, 한글, 알파벳 순서대로 정렬
     func customSort(recipients: [String]) -> [String] {
         return recipients.sorted { (lhs: String, rhs: String) -> Bool in
-            func isKorean(_ string: String) -> Bool {
+            func isKoreanConsonant(_ string: String) -> Bool {
                 for scalar in string.unicodeScalars {
-                    if CharacterSet(charactersIn: "가"..."힣").contains(scalar) {
+                    if scalar.value >= 0x3131 && scalar.value <= 0x314E {
                         return true
                     }
                 }
+                
+                return false
+            }
+
+            func isKoreanSyllable(_ string: String) -> Bool {
+                for scalar in string.unicodeScalars {
+                    if scalar.value >= 0xAC00 && scalar.value <= 0xD7A3 {
+                        return true
+                    }
+                }
+                
                 return false
             }
             
-            func isNumber(_ string: String) -> Bool {
-                return string.rangeOfCharacter(from: CharacterSet.decimalDigits) != nil
+            // 우선순위 -> 숫자: 0, 한글 초성: 1, 한글 음절: 2, 그 외: 3
+            let lhsPriority = isKoreanConsonant(lhs) ? 1 : isKoreanSyllable(lhs) ? 2 : 3
+            let rhsPriority = isKoreanConsonant(rhs) ? 1 : isKoreanSyllable(rhs) ? 2 : 3
+            
+            if lhsPriority == rhsPriority {
+                if lhsPriority == 1 || lhsPriority == 2 {
+                    return lhs < rhs
+                } else {
+                    return lhs.lowercased() < rhs.lowercased()
+                }
+            } else {
+                return lhsPriority < rhsPriority
             }
-            
-            let lhsPriority = (isNumber(lhs) ? 0 : isKorean(lhs) ? 1 : 2)
-            let rhsPriority = (isNumber(rhs) ? 0 : isKorean(rhs) ? 1 : 2)
-            
-            // 왼쪽 String과 오른쪽 String을 비교하여 숫자, 한글, 알파벳 순으로 정렬
-            return lhsPriority == rhsPriority ? lhs < rhs : lhsPriority < rhsPriority
         }
     }
     
