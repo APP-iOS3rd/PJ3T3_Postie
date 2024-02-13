d4e481919afaca5a29f41841cd93da18cff50109
hjsupernova <kem08120506@gmail.com>
2024-02-13 14:37:49 +0900

    Refactor: AddLetterView 편지 저장 및 수정 함수 ViewModel로 이동

--- PJ3T3_Postie/Core/Home/View/AddLetterView.swift
+++ PJ3T3_Postie/Core/Home/View/AddLetterView.swift
@@ -68,10 +68,9 @@ struct AddLetterView: View {
                 Button {
                     Task {
                         if let letter = letter {
-                            await editLetter(letter: letter)
-
+                            await addLetterViewModel.editLetter(letter: letter, letterPhotos: letterPhotos)
                         } else {
-                            await addLetter()
+                            await addLetterViewModel.addLetter(isReceived: isReceived)
                         }
 
                         dismiss()
@@ -123,110 +122,8 @@ struct AddLetterView: View {
             }
         }
         .onAppear {
-            if let letter = letter {
-                if isReceived {
-                    addLetterViewModel.sender = letter.writer
-                } else {
-                    addLetterViewModel.receiver = letter.recipient
-                }
-                addLetterViewModel.date = letter.date
-                addLetterViewModel.text = letter.text
-                addLetterViewModel.summary = letter.summary
-                addLetterViewModel.images = letterPhotos?.map { $0.image } ?? []
-            }
+            addLetterViewModel.updateLetterInfoFromLetter(letter, isReceived: isReceived, letterPhotos: letterPhotos)
         }
-    }
-    
-    /// 이미 작성된 편지를 수정합니다.
-    /// - Parameter letter: 작성된 편지
-    ///
-    /// 1. Firestore에 저장된 편지 데이터를 수정
-    /// 2. Firestorage에 저장된 편지 이미지를 수정 후 불러와 데이터 상태 업데이트
-    /// 3. FirestoreManager의 @Published letter 변수 업데이트 ( AddView, DetailView 연동을 위해서 )
-    /// 4. `firestoreManager.fetchAllLetters()`을 홈뷰, 디테일뷰 데이터 상태 업데이트
-    private func editLetter(letter: Letter) async {
-        firestoreManager.editLetter(
-            documentId: letter.id,
-            writer: addLetterViewModel.sender,
-            recipient: addLetterViewModel.receiver,
-            summary: addLetterViewModel.summary,
-            date: addLetterViewModel.date,
-            text: addLetterViewModel.text,
-            isReceived: isReceived,
-            isFavorite: letter.isFavorite
-        )
-
-        if var savedLetterPhotos = letterPhotos {
-            var uiImagesAndFullPaths = savedLetterPhotos.map { (image: $0.image , fullPath: $0.fullPath)}
-
-            for uiImageAndFullPath in uiImagesAndFullPaths {
-                if !addLetterViewModel.images.contains(uiImageAndFullPath.image) {
-                    if let index = uiImagesAndFullPaths.firstIndex(where: { $0 == uiImageAndFullPath }) {
-                        uiImagesAndFullPaths.remove(at: index)
-                        storageManager.deleteItem(fullPath: uiImageAndFullPath.fullPath)
-                    }
-                }
-            }
-
-            for image in addLetterViewModel.images {
-                if !uiImagesAndFullPaths.map( { $0.image } ).contains(image) {
-                    do {
-                        try await storageManager.saveUIImage(images: [image], docId: letter.id)
-                    } catch {
-                        // TODO: ERROR 처리 필요
-                        print("DEBUG: 이미지 저장 실패")
-                    }
-                }
-            }
-
-            storageManager.images.removeAll()
-            
-            storageManager.listAllFile(docId: letter.id)
-        }
-
-        firestoreManager.letter = Letter(
-            id: letter.id,
-            writer: addLetterViewModel.sender,
-            recipient: addLetterViewModel.receiver,
-            summary: addLetterViewModel.summary,
-            date: addLetterViewModel.date,
-            text: addLetterViewModel.text,
-            isReceived: isReceived,
-            isFavorite: letter.isFavorite
-        )
-
-        firestoreManager.fetchAllLetters()
-    }
-    
-    /// 편지를 추가합니다
-    ///
-    /// 1. Firestore에 편지 추가
-    /// 2. 편지 이미지 값이 존재하면 Firestorage에 이미지 추가
-    /// 3. 모든 편지 불러와서 상태 업데이트
-    private func addLetter() async {
-        await firestoreManager.addLetter(
-            writer: isReceived ? addLetterViewModel.sender : AuthManager.shared.currentUser?.fullName ?? "유저",
-            recipient: isReceived ? AuthManager.shared.currentUser?.fullName ?? "유저" : addLetterViewModel.receiver,
-            summary: addLetterViewModel.summary,
-            date: addLetterViewModel.date,
-            text: addLetterViewModel.text,
-            isReceived: isReceived,
-            isFavorite: false
-        )
-
-        if !addLetterViewModel.images.isEmpty {
-            do {
-                try await storageManager.saveUIImage(
-                    images: addLetterViewModel.images,
-                    docId: firestoreManager.docId
-                )
-            } catch {
-                // TODO: Error 처리 필요
-                print("DEBUG: Failed to save UIImages to Firestore: \(error)")
-            }
-        }
-
-        firestoreManager.fetchAllLetters()
     }
 }
 
--- PJ3T3_Postie/Core/Home/ViewModel/AddLetterViewModel.swift
+++ PJ3T3_Postie/Core/Home/ViewModel/AddLetterViewModel.swift
@@ -6,9 +6,11 @@
 //
 
 import Foundation
+import SwiftUI
 import UIKit
 
-class AddLetterViewModel: ObservableObject {
+
+final class AddLetterViewModel: ObservableObject {
     @Published var sender: String = ""
     @Published var receiver: String = ""
     @Published var date: Date = .now
@@ -26,10 +28,118 @@ class AddLetterViewModel: ObservableObject {
 
     func removeImage(at index: Int) {
         images.remove(at: index)
+        FirestoreManager.shared.deleteLetter(documentId: "df")
     }
 
     func showUIImagePicker(sourceType: UIImagePickerController.SourceType) {
         imagePickerSourceType = sourceType
         showUIImagePicker = true 
+    }
+
+    /// 이미 작성된 편지를 수정합니다.
+    /// - Parameter letter: 작성된 편지
+    ///
+    /// 1. Firestore에 저장된 편지 데이터를 수정
+    /// 2. Firestorage에 저장된 편지 이미지를 수정 후 불러와 데이터 상태 업데이트
+    /// 3. FirestoreManager의 @Published letter 변수 업데이트 ( AddView, DetailView 연동을 위해서 )
+    /// 4. `firestoreManager.fetchAllLetters()`을 홈뷰, 디테일뷰 데이터 상태 업데이트
+    func editLetter(letter: Letter, letterPhotos: [LetterPhoto]?) async {
+        FirestoreManager.shared.editLetter(
+            documentId: letter.id,
+            writer: sender,
+            recipient: receiver,
+            summary: summary,
+            date: date,
+            text: text,
+            isReceived: letter.isReceived,
+            isFavorite: letter.isFavorite
+        )
+
+        if var savedLetterPhotos = letterPhotos {
+            var uiImagesAndFullPaths = savedLetterPhotos.map { (image: $0.image , fullPath: $0.fullPath)}
+
+            for uiImageAndFullPath in uiImagesAndFullPaths {
+                if !images.contains(uiImageAndFullPath.image) {
+                    if let index = uiImagesAndFullPaths.firstIndex(where: { $0 == uiImageAndFullPath }) {
+                        uiImagesAndFullPaths.remove(at: index)
+                        StorageManager.shared.deleteItem(fullPath: uiImageAndFullPath.fullPath)
+                    }
+                }
+            }
+
+            for image in images {
+                if !uiImagesAndFullPaths.map( { $0.image } ).contains(image) {
+                    do {
+                        try await StorageManager.shared.saveUIImage(images: [image], docId: letter.id)
+                    } catch {
+                        // TODO: ERROR 처리 필요
+                        print("DEBUG: 이미지 저장 실패")
+                    }
+                }
+            }
+
+            StorageManager.shared.images.removeAll()
+
+            StorageManager.shared.listAllFile(docId: letter.id)
+        }
+
+        FirestoreManager.shared.letter = Letter(
+            id: letter.id,
+            writer: sender,
+            recipient: receiver,
+            summary: summary,
+            date: date,
+            text: text,
+            isReceived: letter.isReceived,
+            isFavorite: letter.isFavorite
+        )
+
+        FirestoreManager.shared.fetchAllLetters()
+    }
+    
+    /// 편지를 추가합니다
+    ///
+    /// 1. Firestore에 편지 추가
+    /// 2. 편지 이미지 값이 존재하면 Firestorage에 이미지 추가
+    /// 3. 모든 편지 불러와서 상태 업데이트
+    func addLetter(isReceived: Bool) async {
+        await FirestoreManager.shared.addLetter(
+            writer: isReceived ? sender : AuthManager.shared.currentUser?.fullName ?? "유저",
+            recipient: isReceived ? AuthManager.shared.currentUser?.fullName ?? "유저" : receiver,
+            summary: summary,
+            date: date,
+            text: text,
+            isReceived: isReceived,
+            isFavorite: false
+        )
+
+        if !images.isEmpty {
+            do {
+                try await StorageManager.shared.saveUIImage(
+                    images: images,
+                    docId: FirestoreManager.shared.docId
+                )
+            } catch {
+                // TODO: Error 처리 필요
+                print("DEBUG: Failed to save UIImages to Firestore: \(error)")
+            }
+        }
+
+        FirestoreManager.shared.fetchAllLetters()
+    }
+    
+    func updateLetterInfoFromLetter(_ letter: Letter?, isReceived: Bool, letterPhotos: [LetterPhoto]?) {
+        guard let letter = letter else { return }
+
+        if isReceived {
+            sender = letter.writer
+        } else {
+            receiver = letter.recipient
+        }
+
+        date = letter.date
+        text = letter.text
+        summary = letter.summary
+        images = letterPhotos?.map { $0.image } ?? []
     }
 }
