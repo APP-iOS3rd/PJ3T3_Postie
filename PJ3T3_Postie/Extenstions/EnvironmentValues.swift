9df359dd16e5f4db4309d0d4497fd1fbfd04baf0
Eunice <jes.e.0927@gmail.com>
2024-02-13 19:20:17 +0900

    Feat: 최상단 뷰를 받아오기 위한 extension 작성

--- PJ3T3_Postie/Extenstions/EnvironmentValues.swift
+++ PJ3T3_Postie/Extenstions/EnvironmentValues.swift
@@ -5,4 +5,23 @@
 //  Created by Eunsu JEONG on 2/13/24.
 //
 
-import Foundation
+import SwiftUI
+
+extension EnvironmentValues {
+    var window: UIWindow? {
+        get {
+            return self[WindowKey.self].value
+        }
+        set {
+            self[WindowKey.self] = .init(value: newValue)
+        }
+    }
+}
+
+struct WindowKey: EnvironmentKey {
+    struct Value {
+        weak var value: UIWindow?
+    }
+    
+    static let defaultValue: Value = .init(value: nil)
+}
