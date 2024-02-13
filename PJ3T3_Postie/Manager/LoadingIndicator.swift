f8548458d5bc8a46af7c66b7c08fe32f6acfeb8d
Eunice <jes.e.0927@gmail.com>
2024-02-13 23:46:50 +0900

    Add: 로딩 뷰 구현을 위한 파일 생성

---
+++ PJ3T3_Postie/Manager/LoadingIndicator.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingIndicator.swift
+//  PJ3T3_Postie
+//
+//  Created by Eunsu JEONG on 2/13/24.
+//
+
+import Foundation
+
+class LoadingIndicator {
+    static func showLoading() {
+        DispatchQueue.main.async {
+            // ...
+        }
+    }
+    
+    static func hideLoading() {
+        DispatchQueue.main.async {
+            // ...
+        }
+    }
+}
