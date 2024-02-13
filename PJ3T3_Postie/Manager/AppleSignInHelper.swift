30c7931e06ccb3808b7294b8e9e5d71ffd732821
Eunice <jes.e.0927@gmail.com>
2024-02-13 21:39:37 +0900

    Feat: Apple 회원 탈퇴 코드 구현
    Apple 회원가입시 token이 발행되는데 회원 탈퇴 할 때에는 반드시 이 토큰을 비활성화 해야 한다.
    전체적인 process는 Apple 로그인과 유사하다.
    로그인 과정의 제일 마지막에 revokeToken하여 deleteAccount한다는 점만 다르다.
    추후 로그인, 회원탈퇴에 중복된 코드는 따로 정리 할 예정이다.
    window는 뷰에서 필요할 때 onAppear로 값을 넣어준다.

--- PJ3T3_Postie/Manager/AppleSignInHelper.swift
+++ PJ3T3_Postie/Manager/AppleSignInHelper.swift
@@ -12,6 +12,7 @@ final class AppleSignInHelper: NSObject, ObservableObject {
     static let shared = AppleSignInHelper()
     private var nonce = ""
     var cryptoUtils: CryptoUtils?
+    var window: UIWindow?
     
     private override init() { }
     
@@ -57,5 +58,88 @@ final class AppleSignInHelper: NSObject, ObservableObject {
         case .failure(let failure):
             print(failure.localizedDescription)
         }
+    }
+    
+    func deleteCurrentAppleUser() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        
+        self.cryptoUtils = CryptoUtils()
+        
+        guard let cryptoUtils = cryptoUtils else {
+            print(#function, "Failed to ")
+            return
+        }
+        
+        nonce = cryptoUtils.randomNonceString
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = cryptoUtils.nonce
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        
+        authorizationController.delegate = self
+        
+        if let window = window {
+            let provider = ContextProvider(window: window)
+            authorizationController.presentationContextProvider = provider
+        }
+        
+        authorizationController.performRequests()
+    }
+}
+
+extension AppleSignInHelper: ASAuthorizationControllerDelegate {
+    func authorizationController(controller: ASAuthorizationController,
+                                 didCompleteWithAuthorization authorization: ASAuthorization) {
+        guard let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential else {
+            print(#function, "Unable to retrieve AppleIDCredential")
+            return
+        }
+        
+        guard let appleAuthCode = appleIDCredential.authorizationCode else {
+            print(#function, "Unable to fetch authorization code")
+            return
+        }
+        
+        guard let authCodeString = String(data: appleAuthCode, encoding: .utf8) else {
+            print(#function, "Unable to serialize auth code string from data: \(appleAuthCode.debugDescription)")
+            return
+        }
+        
+        guard let appleIDToken = appleIDCredential.identityToken else {
+            print(#function, "Unable to fetch identity token")
+            return
+        }
+        
+        guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+            print(#function, "Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+            return
+        }
+        
+        guard let fullName = appleIDCredential.fullName else {
+            print(#function, "Unalbe to get PersonNameComponents: \(appleIDToken.debugDescription)")
+            return
+        }
+        
+        let appleUser = AppleUser(token: idTokenString, nonce: nonce, fullName: fullName)
+        
+        //회원 탈퇴를 위해서는 재인증이 필요하다. 재인증을 위한 credential을 생성 하기 위해서는 로그인을 다시 해야 한다.
+        AuthManager.shared.signInwithApple(user: appleUser)
+        
+        Task {
+            await AuthManager.shared.deleteAppleAccount(authCodeString: authCodeString)
+        }
+    }
+}
+
+final class ContextProvider: NSObject, ASAuthorizationControllerPresentationContextProviding {
+    private var window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        window
     }
 }
