6974495739e69decaac5c16b34f14c6009f828f9
Eunice <jes.e.0927@gmail.com>
2024-02-15 15:57:15 +0900

    Add: FirestoreNoticeManager와 OfficialLetter Model을 추가

---
+++ PJ3T3_Postie/Manager/FirestoreNoticeManager.swift
@@ -0,0 +1,45 @@
+//
+//  FirestoreOLManager.swift
+//  PJ3T3_Postie
+//
+//  Created by Eunsu JEONG on 2/15/24.
+//
+
+import FirebaseFirestore
+
+final class FirestoreNoticeManager: ObservableObject {
+    static let shared = FirestoreNoticeManager()
+    @Published var notice: [Shop] = []
+    
+    private init() { }
+    
+    func fetchAllShops() {
+        let docRef = Firestore.firestore().collection("shops")
+        
+        docRef.getDocuments { snapshot, error in
+            guard error == nil else {
+                print(error?.localizedDescription ?? "Undefined error")
+                return
+            }
+            
+            self.shops.removeAll()
+            
+            guard let snapshot = snapshot else {
+                print("\(#function): No snapshot \(String(describing: error?.localizedDescription))")
+                return
+            }
+            
+            for document in snapshot.documents {
+                do {
+                    let data = try document.data(as: Shop.self)
+                    
+                    self.shops.append(data)
+                } catch {
+                    print(#function, error.localizedDescription)
+                }
+            }
+            
+            print("Shop fetch success")
+        }
+    }
+}
---
+++ PJ3T3_Postie/Model/OfficialLetter.swift
@@ -0,0 +1,8 @@
+//
+//  OfficialLetter.swift
+//  PJ3T3_Postie
+//
+//  Created by Eunsu JEONG on 2/15/24.
+//
+
+import Foundation
