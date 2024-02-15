a6019cf5ccd9572ebe25f525da55408cf4d6caf2
Eunice <jes.e.0927@gmail.com>
2024-02-16 03:46:34 +0900

    Feat: 계정 삭제 기능 switch가 구현된 버튼

---
+++ PJ3T3_Postie/Core/Login/View/DeleteAccountButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  DeleteAccountButtonView.swift
+//  PJ3T3_Postie
+//
+//  Created by Eunsu JEONG on 2/16/24.
+//
+
+import SwiftUI
+
+struct DeleteAccountButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeleteAccountButtonView()
+}
