21c346938b0472c9da871975f15b8cf61c16b1eb
Eunice <jes.e.0927@gmail.com>
2024-02-21 11:47:10 +0900

    Add: 닉네임뷰의 재인증을 위한 버튼 파일 추가

---
+++ PJ3T3_Postie/Core/Login/View/ReAuthButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ReAuthButtonView.swift
+//  PJ3T3_Postie
+//
+//  Created by Eunsu JEONG on 2/20/24.
+//
+
+import SwiftUI
+
+struct ReAuthButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReAuthButtonView()
+}
