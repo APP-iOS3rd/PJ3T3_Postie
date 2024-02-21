60e354feae0a40bac45290146b27d45ebdca7d63
Eunice <jes.e.0927@gmail.com>
2024-02-21 12:11:19 +0900

    Feat: 회원가입 중 닉네임 제출 전 앱이 종료되면 계정 재인증해 회원가입 이어가도록 수정
    기존은 회원탈퇴 후 재가입했으나, 유저 이탈을 고려해 재인증 후 바로 가입 하도록 수정

--- PJ3T3_Postie/Core/Login/View/ReAuthButtonView.swift
+++ PJ3T3_Postie/Core/Login/View/ReAuthButtonView.swift
@@ -6,13 +6,49 @@
 //
 
 import SwiftUI
+import AuthenticationServices
 
 struct ReAuthButtonView: View {
+    @ObservedObject var authManager = AuthManager.shared
+    @Binding var showLoading: Bool
+    @Binding var nickname: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("계정 재인증") {
+            switch authManager.provider {
+            case .google:
+                print("Re-Auth Google account")
+                Task {
+                    do {
+                        let credential = try await authManager.signInWithGoogle()
+                        showLoading = true
+                        authManager.authDataResult = try await authManager.signInWithSSO(credential: credential)
+                        showLoading = false
+                    } catch {
+                        print(#function, "Failed to re-auth Google account: \(error)")
+                        showLoading = false
+                    }
+                }
+            case .apple:
+                print("Re-Auth Apple account")
+                AppleSignInHelper.shared.isReAuth = true
+                AppleSignInHelper.shared.reAuthCurrentAppleUser()
+            default:
+                print("Cannot re-auth account")
+                //alert 창 구현
+            }
+        }
+        .customOnChange(authManager.credential) { newValue in
+            if authManager.credential == nil {
+                print(#function, "Canceled to delete account")
+                showLoading = false
+            } else {
+                showLoading = true
+            }
+        }
     }
 }
 
 #Preview {
-    ReAuthButtonView()
+    ReAuthButtonView(showLoading: .constant(false), nickname: .constant("포스티"))
 }
