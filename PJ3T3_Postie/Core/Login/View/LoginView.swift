390854970c94d38b649a592741f5967cab665d69
Eunice <jes.e.0927@gmail.com>
2024-02-22 20:08:39 +0900

    Feat: 로그인 뷰 기기별 사이즈 대응

--- PJ3T3_Postie/Core/Login/View/LoginView.swift
+++ PJ3T3_Postie/Core/Login/View/LoginView.swift
@@ -17,97 +17,99 @@ struct LoginView: View {
     
     var body: some View {
         NavigationStack {
-            ZStack {
-                postieColors.backGroundColor
-                    .ignoresSafeArea()
-                
-                ToFromLabelView()
-                    .padding()
-                
-                VStack {
-                    Text("Postie")
-                        .font(.custom("SourceSerifPro-Black", size: 70))
-                        .foregroundStyle(postieColors.tintColor)
-                        .padding(.bottom, 36)
+            GeometryReader { geometry in
+                ZStack {
+                    postieColors.backGroundColor
+                        .ignoresSafeArea()
                     
-                    Image("postySendingLineColor")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 152)
+                    ToFromLabelView()
+                        .padding()
                     
-                    Text("내 손안의 편지 보관함\n언제 어디서나")
-                        .foregroundStyle(postieColors.dividerColor)
-                        .padding(.bottom, 48)
-                        .multilineTextAlignment(.center)
-
-                    //Google sign in Button
-                    Button {
-                        Task {
-                            do {
-                                let credential = try await authManager.signInWithGoogle()
-                                authManager.authDataResult = try await authManager.signInWithSSO(credential: credential)
-                            } catch {
-                                print(error)
-                            }
-                        }
-                    } label: {
-                        ZStack {
-                            //Apple 로그인 버튼과 구글 로그인 버튼의 cornerRadius 일치를 위한 배경색
-                            //stroke를 이미지 아래에 그리면 이미지가 stoke 라인을 조금 가려서 stoke만 이미지에 overlay함
-                            RoundedRectangle(cornerRadius: 5)
-                                .foregroundStyle(.white)
-                                .shadow(radius: 3, x: 3, y: 3)
-                            
-                            Image("GoogleSignIn")
-                                .resizable()
-                                .scaledToFit()
-                            
-                            RoundedRectangle(cornerRadius: 5)
-                                .stroke(Color(.black), lineWidth: 0.5)
-                                .foregroundStyle(.clear)
-                        }
-                        .frame(height: 54)
-                    }
-                    .padding(.bottom, 10)
-                    
-                    SignInWithAppleButton { request in
-                        appleSignInHelper.signInWithAppleRequest(request)
-                    } onCompletion: { result in
-                        Task {
-                            do {
-                                appleSignInHelper.signInWithAppleCompletion(result)
-                                guard let credential = authManager.credential else {
-                                    print("Unable to fetch credential")
-                                    return
+                    VStack {
+                        Text("Postie")
+                            .font(.custom("SourceSerifPro-Black", size: 70))
+                            .foregroundStyle(postieColors.tintColor)
+                            .padding(.bottom, geometry.size.height > 730 ? 36 : 1)
+                        
+                        Image("postySendingLineColor")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 152)
+                        
+                        Text("내 손안의 편지 보관함\n언제 어디서나")
+                            .foregroundStyle(postieColors.dividerColor)
+                            .padding(.bottom, geometry.size.height > 700 ? 48 : 28)
+                            .multilineTextAlignment(.center)
+                        
+                        //Google sign in Button
+                        Button {
+                            Task {
+                                do {
+                                    let credential = try await authManager.signInWithGoogle()
+                                    authManager.authDataResult = try await authManager.signInWithSSO(credential: credential)
+                                } catch {
+                                    print(error)
                                 }
-                                authManager.authDataResult = try await AuthManager.shared.signInWithSSO(credential: credential)
-                            } catch {
-                                print(error)
+                            }
+                        } label: {
+                            ZStack {
+                                //Apple 로그인 버튼과 구글 로그인 버튼의 cornerRadius 일치를 위한 배경색
+                                //stroke를 이미지 아래에 그리면 이미지가 stoke 라인을 조금 가려서 stoke만 이미지에 overlay함
+                                RoundedRectangle(cornerRadius: 5)
+                                    .foregroundStyle(.white)
+                                    .shadow(radius: 3, x: 3, y: 3)
+                                
+                                Image("GoogleSignIn")
+                                    .resizable()
+                                    .scaledToFit()
+                                
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(Color(.black), lineWidth: 0.5)
+                                    .foregroundStyle(.clear)
+                            }
+                            .frame(height: geometry.size.height > 700 ? 54 : 44)
+                        }
+                        .padding(.bottom, 10)
+                        
+                        SignInWithAppleButton { request in
+                            appleSignInHelper.signInWithAppleRequest(request)
+                        } onCompletion: { result in
+                            Task {
+                                do {
+                                    appleSignInHelper.signInWithAppleCompletion(result)
+                                    guard let credential = authManager.credential else {
+                                        print("Unable to fetch credential")
+                                        return
+                                    }
+                                    authManager.authDataResult = try await AuthManager.shared.signInWithSSO(credential: credential)
+                                } catch {
+                                    print(error)
+                                }
                             }
                         }
+                        .frame(height: geometry.size.height > 700 ? 54 : 44)
+                        .signInWithAppleButtonStyle(isThemeGroupButton == 4 ? .white : .black)
+                        .shadow(radius: 3, x: 3, y: 3)
+                        .padding(.bottom, 19)
+                        
+                        //Email sign in: 테스트용 이메일을 사용하기 위한 것으로 배포시 삭제 예정입니다.
+//                        NavigationLink {
+//                            EmailLoginView()
+//                        } label: {
+//                            HStack(spacing: 0) {
+//                                Text("테스트용 이메일 계정으로")
+//                                    .foregroundColor(postieColors.tabBarTintColor)
+//                                
+//                                Text("로그인 하기")
+//                                    .foregroundColor(postieColors.tintColor)
+//                                    .bold()
+//                                    .padding(.leading, 4)
+//                            }
+//                        }
+//                        .padding(.bottom, 10)
                     }
-                    .frame(height: 54)
-                    .signInWithAppleButtonStyle(isThemeGroupButton == 4 ? .white : .black)
-                    .shadow(radius: 3, x: 3, y: 3)
-                    .padding(.bottom, 19)
-                    
-                    //Email sign in: 테스트용 이메일을 사용하기 위한 것으로 배포시 삭제 예정입니다.
-                    NavigationLink {
-                        EmailLoginView()
-                    } label: {
-                        HStack(spacing: 0) {
-                            Text("테스트용 이메일 계정으로")
-                                .foregroundColor(postieColors.tabBarTintColor)
-                            
-                            Text("로그인 하기")
-                                .foregroundColor(postieColors.tintColor)
-                                .bold()
-                                .padding(.leading, 4)
-                        }
-                    }
-                    .padding(.bottom, 10)
+                    .padding(.horizontal, 32)
                 }
-                .padding(.horizontal, 32)
             }
         }
     }
